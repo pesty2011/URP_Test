db86471a46070acfc5063ebedfaf7a7b37fc5833
rob.anderson <rob.anderson@seasungames.com>
2024-04-17 22:38:52 -0700

    initial checkin of code.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    GameObject Target { get; set; }
+    Vector3 cameraVelocity { get; set; }
+    Camera baseCamera { get; set; }
+
+    public bool followTarget { get; set; }
+    public bool trackZPlane { get; set; }
+
+    public Vector3 desiredPositionOffset = new Vector3(0, 2.0f, 7.0f);
+
+    public Vector3 DesiredPosition
+    {
+        get
+        {
+            UpdateWorldPosition();
+            return desiredPositionOffset;
+        }
+    }
+
+    private Vector3 desiredPosition = Vector3.zero;
+    public Vector3 LookAtOffset = new Vector3(0.0f, 2.8f, 0.0f);
+	public float SmoothedSpeed = 0.125f;
+
+	#region Camera Spring
+	
+	[SerializeField] public bool bSpringCamera = false;
+
+	/// <summary>
+	/// Physics coefficient which controls the influence of the camera's position
+	/// over the spring force.  The stiffer the spring, the closer it will stay to
+	/// the chased object
+	/// </summary>
+	[SerializeField] public float Stiffness = 4000f;
+
+	/// <summary>
+	/// Physics coefficient which approximates internal friction of the sprint.
+	/// Sufficient damping will prevent the spring from oscillating infinitely.
+	/// </summary>
+	[SerializeField] public float Damping = 600f;
+
+
+
+	/// <summary>
+	/// Mass of the camera body.  Heavier objects require stiffer springs with less
+	/// damping to move at the same rate as light objects.
+	/// </summary>
+	[SerializeField] public float Mass = 1f;
+
+	#endregion
+
+
+
+	static float pixelToUnits = 100.0f;		// this value should come from target sprite-renderer component.
+	public float RoundToNearestPixel(float unityUnits)
+	{
+		float valueInPixels = unityUnits * pixelToUnits;
+		valueInPixels = Mathf.Round(valueInPixels);
+		float roundedUnityUnits = valueInPixels * (1 / pixelToUnits);
+		return roundedUnityUnits;
+
+	}
+
+    /// <summary>
+    /// calculates the camera position based upon the target transform.
+    /// </summary>
+    private void UpdateWorldPosition()
+    {
+        if (Target != null)
+        {
+            float targetXPos = Target.transform.position.x;
+            Vector3 cameraPos = baseCamera.transform.position;
+            cameraPos.x = targetXPos;
+
+            desiredPosition = cameraPos;
+        }
+    }
+
+
+	private void GetDesiredPosition()
+	{
+		if (Target != null)
+		{
+			float targetXPos = Target.transform.position.x;
+			float roundXPos = RoundToNearestPixel(targetXPos);
+
+			Vector3 cameraPos = baseCamera.transform.position;
+			cameraPos.x = roundXPos;
+
+			desiredPosition = cameraPos;
+		}
+	}
+
+	/// <summary>
+	/// Forces camera to be at desired position and to stop moving.  This is useful
+	/// when the chased object is first created or after it has been killed or similar
+	/// 
+	/// Failing to call this after a large change to the chased object's position will
+	/// result in the camera quickly flying across the world
+	/// </summary>
+	public void Reset()
+	{
+        UpdateWorldPosition();
+	}
+
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        baseCamera = Camera.main;
+
+		Target = GameObject.FindGameObjectWithTag("Player");
+        followTarget = true;
+
+        Reset();
+	}
+
+
+	/// <summary>
+	/// Moves the camera from its current position towards the desired offset
+	/// behind the chased object.  The camera's movement is controlled by a
+	/// simple physical spring attached to the camera and anchored to the
+	/// desired position.
+	/// </summary>
+	private void Update()
+	{
+        float deltaTime = Time.deltaTime;
+
+        //UpdateWorldPosition();
+		GetDesiredPosition();
+
+
+
+
+		if (followTarget == true)
+        {
+
+			if (bSpringCamera)
+			{
+				// calculate spring
+				Vector3 stretch = baseCamera.transform.position - desiredPosition;
+				Vector3 force = -Stiffness * stretch - Damping * cameraVelocity;
+
+				// apply acceleration
+				Vector3 acceleration = force / Mass;
+				cameraVelocity += acceleration * Time.deltaTime;
+
+				Vector3 targetPosition = baseCamera.transform.position + cameraVelocity;
+				baseCamera.transform.position = targetPosition;
+			}
+			else
+			{
+				// Smoothly move the camera towards the target position
+				baseCamera.transform.position = Vector3.Lerp(baseCamera.transform.position, desiredPosition, 5.0f * deltaTime);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 m_input;
+    private CharacterController m_controller;
+    private Vector3 m_moveDir = Vector3.zero;
+    private float m_verticalVelocity;
+    private float m_gravityValue = 8.81f;
+    public float m_turnSmoothTime = 0.1f;
+    public float m_turnSmoothVelocity;
+    public Transform m_cam;
+    public Camera m_camera;
+    public float m_Force = 3.0f;    
+	public float m_JumpForce = 8f;
+	public float m_RotationSpeed = 500f;
+
+    
+    public Vector3 m_lastMoveDir = Vector3.zero;
+    public float m_smoothTime = 0f;
+    public float m_targetSpeed = 0;
+	public Vector3 m_PlayerVelocity;
+	[SerializeField] public float m_fSpeed = 5.0f;
+
+
+	private void Awake()
+	{
+        m_controller = GetComponent<CharacterController>();
+        m_camera = Camera.main;
+		m_cam = m_camera.transform;
+	}
+
+
+
+	void HandleMovement()
+	{
+		if (m_moveDir.magnitude >= 0.1f)
+		{
+			float targetAngle = Mathf.Atan2(m_moveDir.x, m_moveDir.z) * Mathf.Rad2Deg;
+			float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref m_RotationSpeed, 0.1f);
+			transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+			Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+			m_PlayerVelocity = moveDirection * m_fSpeed;
+
+		}
+	}
+
+
+	void ApplyGravity()
+	{
+		float dTime = Time.fixedDeltaTime;
+
+		bool groundedPlayer = m_controller.isGrounded;
+		if (groundedPlayer == false)
+		{
+			m_verticalVelocity -= m_gravityValue * Time.deltaTime;
+		}
+
+
+		if (groundedPlayer && m_verticalVelocity < 0f)
+		{
+			m_verticalVelocity = 0f;
+		}
+	}
+
+
+
+
+	/// <summary>
+	/// Callback event from the input action library.  This will send in 
+	/// new information regarding direction the player is moving.
+	/// </summary>
+	/// <param name="context"></param>
+	public void Move(InputAction.CallbackContext context)
+	{
+		m_input = context.ReadValue<Vector2>();
+		m_moveDir = new Vector3(m_input.x, 0.0f, m_input.y * 2.0f);
+	}
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dTime = Time.deltaTime;
+
+		ApplyGravity();
+
+		Vector3 move = m_moveDir;
+        move = transform.TransformDirection(move.normalized);
+        if (move == Vector3.zero)
+        {
+            m_smoothTime = 0.8f;
+            m_targetSpeed = 0f;
+        }
+        else
+        {
+            m_lastMoveDir = move;
+            m_targetSpeed = 1.0f;
+            m_smoothTime = 0.3f;
+        }
+
+        m_moveDir.y = m_verticalVelocity;
+		m_controller.Move(m_moveDir * m_fSpeed * dTime);
+	}
+}
